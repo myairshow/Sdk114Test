649c9d5fca69ccc704f8945c4215ab1f4db1bae0
myairshow <www.airshow@qq.com>
2018-04-10 23:23:03 +0800

    log

--- app/src/main/java/cn/icartoons/icartoon/MainActivity.kt
+++ app/src/main/java/cn/icartoons/icartoon/MainActivity.kt
@@ -1,12 +1,19 @@
 package cn.icartoons.icartoon
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.besttone.BiAgent
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            BiAgent.bindUserInfo(this,"70721837","13328322806")
+        }
+        eventButton.setOnClickListener {
+            BiAgent.onEvent(this,"E0001","")
+        }
     }
 }
